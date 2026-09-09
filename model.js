4a2937b26d40c204142922a2ddaa3212e90c437d
Sana <suhanasana056@gmail.com>
2026-09-09 10:27:56 +0530

    Done

---
+++ model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const DataBase = mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    Discription:{
+        type:String,
+        required:true
+    },
+    Status:{
+        type:Boolean,
+        default:false
+    },
+    CreatesAt:{
+        type:Date,
+        default:Date.now
+    }
+})
+GPUShaderModule.exports = mongoose.model("database",DataBase)
